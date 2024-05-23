c6ff290f2102d292933032888afea54a733bb6c4
Lisa Wild <lisakwild@gmail.com>
2024-05-23 08:18:46 -0700

    chore: add heart icon to the favourite button component

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -2,12 +2,13 @@ import React, { useCallback, useState } from 'react';
 
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
+import FavBadge from './FavBadge';
 
 function PhotoFavButton() {
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavBadge />
       </div>
     </div>
   );
