a445d65ca7337369e94345b200dda9657505052d
Lisa Wild <lisakwild@gmail.com>
2024-05-25 21:58:50 -0700

    chore: use useEffect and fetch requests to populate the app with API derived data; include a proxy to ensure the API request functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,43 @@
-import React, {useReducer} from 'react';
+import React, {useReducer, useEffect} from 'react';
 import HomeRoute from './routes/HomeRoute';
-import photos from '../src/mocks/photos'
-import topics from '../src/mocks/topics';
 import PhotoDetailsModal from './routes/PhotoDetailsModal';
 import './App.scss';
-import { useApplicationData} from './hooks/useApplicationData';
+import { useApplicationData, ACTIONS} from './hooks/useApplicationData';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const [state, dispatch] = useReducer(useApplicationData, {displayModal: false, photoClicked: {}, favouritedPhotos: [],})
+  
+  const initialState = {
+
+    displayModal: false, 
+    photoClicked: {}, 
+    favouritedPhotos: [], 
+    photoData: [], 
+    topicData: []
+  }
+
+  const [state, dispatch] = useReducer(useApplicationData, initialState)
   // console.log(state.displayModal);
+
+  useEffect(() => {
+    fetch('/api/photos')
+    .then(res => res.json())
+    .then(data => dispatch({type: ACTIONS.SET_PHOTO_DATA, payload: data}));
+  }, [])
+
+  useEffect(() => {
+    fetch('/api/topics')
+    .then(res => res.json())
+    .then(data => dispatch({type: ACTIONS.SET_TOPIC_DATA, payload: data}));
+  }, [])
+  
 
 
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} state={state} dispatch={dispatch}/>
-      <div>{state.displayModal && <PhotoDetailsModal photos={photos} state={state} dispatch={dispatch}/>}</div>
+      <HomeRoute photos={state.photoData} topics={state.topicData} state={state} dispatch={dispatch}/>
+      <div>{state.displayModal && <PhotoDetailsModal photos={state.photoData} state={state} dispatch={dispatch}/>}</div>
     </div>
   );
 };
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,10 +1,16 @@
+import React from "react"
+import { useEffect } from "react"
+
 export const ACTIONS = {
   FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
   FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
   DISPLAY_MODAL: 'DISPLAY_MODAL',
-  CLOSE_MODAL: 'CLOSE_MODAL'
+  CLOSE_MODAL: 'CLOSE_MODAL',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
 }
 
+//should any reference to the previous state be prev?
 export const useApplicationData = (state, action) => {
   switch (action.type) {
     case 'FAV_PHOTO_ADDED':
@@ -15,10 +21,16 @@ export const useApplicationData = (state, action) => {
       return {...state, displayModal: true, photoClicked: action.payload}
     case 'CLOSE_MODAL':
       return {...state, displayModal: false}
+    case 'SET_PHOTO_DATA':
+      return {...state, photoData: action.payload}
+    case 'SET_TOPIC_DATA':
+      return {...state, topicData: action.payload}
     default:
       throw new Error(`invalid action type ${action.type}`);
   }
 }
+
+
 
 export const updateFavourites = (photoId, state, dispatch) => {
   // console.log("photo being liked/unliked", photoId)
